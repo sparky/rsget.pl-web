d46d02bbab96c3ea132fe86ea3989cfda712dc44
sparky <sparky@pld-linux.org>
2010-04-08 00:21:19 +0200

    - mechanisms to easily download new plugins

---
+++ plugins_db.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+#
+use strict;
+use warnings;
+use DBI;
+use Digest::MD5 qw(md5_hex);
+
+my $dbfile = "rsget.pl/download/plugins.sqlite3.db";
+unlink $dbfile;
+
+my $dbh = DBI->connect( "dbi:SQLite:dbname=$dbfile", "", "" );
+
+$dbh->begin_work();
+$dbh->do(
+"CREATE TABLE plugins(
+	name	VARCHAR(30),
+	md5	CHAR(32),
+	body	TEXT
+)"
+);
+
+my $sth = $dbh->prepare( "INSERT INTO plugins( name, md5, body ) VALUES( ?, ?, ? )" );
+
+chdir "svn";
+foreach my $dir ( qw(Get Video Audio Image Link) ) {
+	foreach my $name ( glob "$dir/*" ) {
+		local $/ = undef;
+
+		open my $fin, "<", $name;
+		my $data = <$fin>;
+		close $fin;
+
+		my $md5 = md5_hex( $data );
+
+		$sth->execute( $name, $md5, $data );
+	}
+}
+
+$sth = undef;
+
+$dbh->commit();
+
+$dbh->do( "vacuum" );
+$dbh->disconnect();
---
+++ src/download/plugins.php
@@ -0,0 +1,40 @@
+<?php
+header( 'Content-type: text/plain' );
+
+if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
+	ob_start("ob_gzhandler");
+else
+	ob_start();
+
+function getters() {
+	$dbh = null;
+	try {
+		$dbh = new PDO("sqlite:plugins.sqlite3.db", "", "" );
+	} catch(PDOException $e) {
+		echo $e->getMessage();
+	}
+	if ( ! $dbh )
+		return;
+
+	$sth = null;
+	$sth = $dbh->prepare( "SELECT name, md5, body FROM plugins" );
+	$sth->execute();
+
+	$result = $sth->fetchAll();
+	foreach ( $result as $row ) {
+		$name = $row[0];
+		$md5 = $row[1];
+		if ( ! isset( $_POST[ $name ] ) || $_POST[ $name ] != $md5 ) {
+			$length = strlen( $row[2] );
+			echo "<plugin:$md5 name=\"$name\">";
+			echo $row[2];
+			echo "</plugin:$md5>\n";
+		}
+	}
+
+	$dbh = null;
+}
+
+echo "<!-- plugins -->\n";
+getters();
+?>
